80174742b9acd444ed731e4f52de09c77921287d
raju <iamraju.eu56@gmail.com>
2023-02-02 22:55:13 +0600

    Finished Error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,6 +4,21 @@ const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
+
+const handleDuplicateError = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  // console.log(value);
+  const message = `Duplicate field value ${value}. please try another`;
+  return new AppError(message, 400);
+};
+
+const handleValidatorError = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  // console.log(error);
+  const message = `Invalid data inpur : ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendDErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -41,9 +56,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendDErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    const { name } = err;
     let error;
-    if (name === 'CastError') error = handleCastErrorDB(err);
+    // console.log(err.name);
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateError(err);
+    if (err.name === 'ValidationError') error = handleValidatorError(err);
 
     sendDErrorProd(error, res);
   }
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Exception error!');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -20,9 +26,16 @@ mongoose
     console.log('Database connection successfully!');
   });
 
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UnhandledReject !!!! shutting server');
+  server.close(() => {
+    process.exit(1);
+  });
 });
